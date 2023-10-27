958d170b185dae1a920ce95af0198d4c132ceb8b
Sasha <alex_penza@list.ru>
2023-10-27 20:57:20 +0300

    experimenting with stuff

--- src/main/java/com/fullstackhub/autokoolweb/models/User.java
+++ src/main/java/com/fullstackhub/autokoolweb/models/User.java
@@ -10,7 +10,7 @@ import java.util.*;
 @Table(name = "users")
 public class User implements UserDetails {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String username;
     private String password;
